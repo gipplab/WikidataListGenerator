84127d947fcd240134fa0858a9fb98c29b285b62
physikerwelt <wiki@physikerwelt.de>
2015-12-16 19:24:14 +0100

    Downgrade to java7 compatibility
    * fix mvn config (remove openjdk8)

--- src/test/java/com/formulasearchengine/wikidatalistgenerator/MainTest.java
+++ src/test/java/com/formulasearchengine/wikidatalistgenerator/MainTest.java
@@ -3,9 +3,9 @@ package com.formulasearchengine.wikidatalistgenerator;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -21,7 +21,12 @@ public class MainTest {
 		final String outFile = temp.getAbsolutePath();
 		String[] args = {"-i", inFile, "-o", outFile, "-l", "de", "-a"};
 		Main.main( args );
-		List<String> lines = Files.readAllLines( Paths.get( outFile ) );
+		Scanner s = new Scanner(temp);
+		List<String> lines = new ArrayList<>();
+		while (s.hasNext()){
+			lines.add(s.next());
+		}
+		s.close();
 		assertEquals( 5, lines.size() );
 		assertEquals( "universum,Q1", lines.get( 0 ) );
 	}
